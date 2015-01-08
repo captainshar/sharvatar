083bde47d6270ef5563ff7020ede672661b2fd2e
captainshar <captainshar@gmail.com>
2015-01-07 23:23:04 -0500

    character database

--- db/migrate/20141205024503_create_characters.rb
+++ db/migrate/20141205024503_create_characters.rb
@@ -2,7 +2,14 @@ class CreateCharacters < ActiveRecord::Migration
   def change
     create_table :characters do |t|
 
-      t.timestamps
+	t.integer	:character_id
+	t.integer	:player_id
+	t.string	:character_name
+	t.string	:larp_class
+	t.string	:race
+	t.integer	:skill_pts
+
+	t.timestamps
     end
   end
 end
