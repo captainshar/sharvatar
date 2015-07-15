f248452af1caa8df9e3fd8b6dbf1dbbc8fccb1b1
Sharon C <captainshar@gmail.com>
2015-07-14 20:04:58 -0400

    view all the players

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -1,8 +1,29 @@
 class PlayersController < ApplicationController
+
+	def index
+		@players = Player.all
+	end
+
+	def show
+		@player = Player.find(params[:id])
+	end
+
 	def new
 	end
 
 	def create
-		render plain: params[:player].inspect
+		#remove this later; shows values
+		#render plain: player_params.inspect
+
+		@player = Player.new(player_params)
+
+		@player.save
+		redirect_to @player
 	end
+
+	private
+		def player_params
+			params.require(:player).permit(:first_name, :last_name, :bio,)
+		end
+
 end
